536390085322273bb2993ed4c8c75273e2d76952
Bogdan's <bogdanmihai453@gmail.com>
2023-10-26 13:47:29 +0200

    biuild

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('*', (req, res) => {
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(port, () => {
-        console.log(`Server is running at https://pleasant-lamb-lab-coat.cyclic.app/${port}`);
+        console.log(`Server is running at https://pleasant-lamb-lab-coat.cyclic.app/`);
     });
 }
 
